e65e9c86fecd3472a0e858d62bd9eed0e246fd0a
GnohChengYi <chengyignoh@gmail.com>
2021-05-26 17:04:06 +0800

    Add wall on touch
    - Touch detection overlaps between creating new walls and dragging existing walls. Should resolve after adding a toolbar to select modes (Create, Move, etc.)
    - Dragging behaviour is very slow and frequently irresponsive (wall position not updated when supposed to, note: device not freezing).

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,22 +1,37 @@
-using Firebase.Leaderboard;
 using System.Collections;
 using System.Collections.Generic;
+using Firebase.Leaderboard;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject wallPrefab;
+
     private LeaderboardController leaderboard;
 
     // Start is called before the first frame update
     void Start()
     {
         leaderboard = GetComponent<LeaderboardController>();
+
+        // TODO remove before production
         leaderboard.AddScore("userIDDDD", "usernammeee", 12344);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.touchCount >= 1)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                Debug.Log("Instatiate wallPrefab");
+                Vector2 touchPos =
+                    Camera.main.ScreenToWorldPoint(touch.position);
+                Instantiate(wallPrefab, touchPos, Quaternion.identity);
+            }
+        }
     }
 }
