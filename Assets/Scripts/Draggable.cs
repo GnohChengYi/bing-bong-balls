57304091a42fdcdad1d66f4dd0ebea7e3c48f27b
GnohChengYi <chengyignoh@gmail.com>
2021-05-26 16:45:44 +0800

    Make walls draggable and snap to grid

---
+++ Assets/Scripts/Draggable.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// based on https://www.youtube.com/watch?v=sXc8baUK3iY
+public class Draggable : MonoBehaviour
+{
+    private Rigidbody2D rigidbody;
+
+    private float deltaX;
+
+    private float deltaY;
+
+    private bool moveAllowed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    if (
+                        GetComponent<Collider2D>() ==
+                        Physics2D.OverlapPoint(touchPos)
+                    )
+                    {
+                        Debug.Log("Object touched");
+
+                        // diff btw touch position & center GameObject
+                        deltaX = touchPos.x - transform.position.x;
+                        deltaY = touchPos.y - transform.position.y;
+                        rigidbody.constraints =
+                            RigidbodyConstraints2D.FreezeRotation;
+                        moveAllowed = true;
+                    }
+                    break;
+                case TouchPhase.Moved:
+                    if (
+                        GetComponent<Collider2D>() ==
+                        Physics2D.OverlapPoint(touchPos) &&
+                        moveAllowed
+                    )
+                    {
+                        Debug.Log("Object dragged");
+                        float x = (float) Math.Round(touchPos.x - deltaX);
+                        float y = (float) Math.Round(touchPos.y - deltaY);
+                        rigidbody.MovePosition(new Vector2(x, y));
+                    }
+                    break;
+                case TouchPhase.Ended:
+                    Debug.Log("Object released");
+                    rigidbody.constraints = RigidbodyConstraints2D.FreezeAll;
+                    moveAllowed = false;
+                    break;
+            }
+        }
+    }
+}
