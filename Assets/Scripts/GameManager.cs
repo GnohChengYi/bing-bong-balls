558ecdf2d9490b51921661ec48ca498eab49ce44
GnohChengYi <chengyignoh@gmail.com>
2021-05-28 13:15:35 +0800

    Add method to record audio data from float[] to .wav file

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,6 +8,8 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     private GameObject wallPrefab;
 
+    private AudioRenderer audioRenderer;
+
     private LeaderboardController leaderboard;
 
     // Start is called before the first frame update
@@ -17,6 +19,11 @@ public class GameManager : MonoBehaviour
 
         // TODO remove before production
         leaderboard.AddScore("userIDDDD", "usernammeee", 12344);
+        audioRenderer = new AudioRenderer();
+        float[] audioData = new float[] {1F, 2F, 3F, 4F, 5F};
+        audioRenderer.Write(audioData);
+        string filename = "temp_music.wav";
+        audioRenderer.Save(filename);
     }
 
     // Update is called once per frame
@@ -27,11 +34,30 @@ public class GameManager : MonoBehaviour
             Touch touch = Input.GetTouch(0);
             if (touch.phase == TouchPhase.Began)
             {
-                Debug.Log("Instatiate wallPrefab");
+                // Debug.Log("Instatiate wallPrefab");
                 Vector2 touchPos =
                     Camera.main.ScreenToWorldPoint(touch.position);
                 Instantiate(wallPrefab, touchPos, Quaternion.identity);
             }
+        }
+    }
+
+    public static GameManager Instance { get; private set; }
+
+    void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy (Instance);
+        }
+        Instance = this;
+    }
+
+    void OnDestroy()
+    {
+        if (Instance == this)
+        {
+            Instance = null;
         }
     }
 }
