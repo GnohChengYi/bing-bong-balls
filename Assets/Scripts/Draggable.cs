0337d47bc22eb890c08d73929b575f0c018f1058
GnohChengYi <chengyignoh@gmail.com>
2021-06-01 17:17:09 +0800

    Fix overdragging issue caused by different screen resolutions

--- Assets/Scripts/Draggable.cs
+++ Assets/Scripts/Draggable.cs
@@ -13,19 +13,13 @@ Draggable
     // TODO handle offset between finger and dragged object
     public void OnBeginDrag(PointerEventData eventData)
     {
-        // there is canvas scaler, so need divide pointer delta by canbas scale to match pointer movement
-        transform.localPosition +=
-            new Vector3(eventData.delta.x, eventData.delta.y, 0) /
-            transform.lossyScale.x;
+        transform.localPosition += GetDeltaVector(eventData);
         transform.SetAsLastSibling();
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        // there is canvas scaler, so need divide pointer delta by canbas scale to match pointer movement
-        transform.localPosition +=
-            new Vector3(eventData.delta.x, eventData.delta.y, 0) /
-            transform.lossyScale.x;
+        transform.localPosition += GetDeltaVector(eventData);
     }
 
     public void OnEndDrag(PointerEventData eventData)
@@ -40,62 +34,11 @@ Draggable
         transform.localPosition = new Vector3(snapX, snapY, 0);
     }
 
-    // private Rigidbody2D rigidbody;
-
-    // private Collider2D collider;
-
-    // private float deltaX;
-
-    // private float deltaY;
-
-    // private bool moveAllowed;
-
-    // // Start is called before the first frame update
-    // void Start()
-    // {
-    //     rigidbody = GetComponent<Rigidbody2D>();
-    //     collider = GetComponent<Collider2D>();
-    // }
-
-    // // Update is called once per frame
-    // void Update()
-    // {
-    //     if (
-    //         Input.touchCount > 0 &&
-    //         GameManager.Instance.mode == GameManager.Mode.MOVE
-    //     )
-    //     {
-    //         Touch touch = Input.GetTouch(0);
-    //         Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
-    //         switch (touch.phase)
-    //         {
-    //             case TouchPhase.Began:
-    //                 if (collider == Physics2D.OverlapPoint(touchPos))
-    //                 {
-    //                     // diff btw touch position & center GameObject
-    //                     deltaX = touchPos.x - transform.position.x;
-    //                     deltaY = touchPos.y - transform.position.y;
-    //                     rigidbody.constraints =
-    //                         RigidbodyConstraints2D.FreezeRotation;
-    //                     moveAllowed = true;
-    //                 }
-    //                 break;
-    //             case TouchPhase.Moved:
-    //                 if (
-    //                     collider == Physics2D.OverlapPoint(touchPos) &&
-    //                     moveAllowed
-    //                 )
-    //                 {
-    //                     float x = (float) Math.Round(touchPos.x - deltaX);
-    //                     float y = (float) Math.Round(touchPos.y - deltaY);
-    //                     rigidbody.MovePosition(new Vector2(x, y));
-    //                 }
-    //                 break;
-    //             case TouchPhase.Ended:
-    //                 rigidbody.constraints = RigidbodyConstraints2D.FreezeAll;
-    //                 moveAllowed = false;
-    //                 break;
-    //         }
-    //     }
-    // }
+    public Vector3 GetDeltaVector(PointerEventData eventData)
+    {
+        float screenSize = Math.Max(Screen.width, Screen.height);
+        float deltaX = eventData.delta.x * 1920 / screenSize;
+        float deltaY = eventData.delta.y * 1920 / screenSize;
+        return new Vector3(deltaX, deltaY, 0);
+    }
 }
