ef8d549ee0d26fb2c1a592be221a323d0ad35302
GnohChengYi <chengyignoh@gmail.com>
2021-06-23 17:11:19 +0800

    Finish basic beginner guide in MainGameScene

--- Assets/Scripts/GuideController.cs
+++ Assets/Scripts/GuideController.cs
@@ -1,24 +1,49 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 // TODO check guide quit guide again, verify guide progress correct
 public class GuideController : MonoBehaviour
 {
     [SerializeField]
     private GameObject guideTextObject;
+    private Text guideTextComponent;
 
-    // 1-indexing
-    private int guideProgress = 1;
+    private int guideProgress = 0;
+
+    [SerializeField]
+    private TextAsset guidesTextAsset;
+    private string[] guideTexts;
 
     private void OnEnable()
     {
-        guideProgress = 1;
+        if (guideTextComponent == null || guideTexts == null)
+        {
+            guideTextComponent = guideTextObject.GetComponent<Text>();
+            guideTexts = guidesTextAsset.text.Split(
+                new string[] { "\r\n\r\n\r\n" }, StringSplitOptions.None);
+        }
+        StartGuide();
+    }
+
+    private void StartGuide()
+    {
+        guideProgress = 0;
+        Debug.Log(guideTextComponent);
+        Debug.Log(guideTexts);
+        guideTextComponent.text = guideTexts[guideProgress];
     }
 
     public void ContinueGuide()
     {
         Debug.Log("ContinueGuide");
+        guideProgress++;
+        if (guideProgress < guideTexts.Length)
+            guideTextComponent.text = guideTexts[guideProgress];
+        else gameObject.SetActive(false);
+
+        // TODO add effects to toolbar (or even simulation panel) accordingly
         switch (guideProgress)
         {
             case 1:
