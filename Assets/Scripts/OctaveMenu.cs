1c87c29393bdab335b2d19b33695c3ffbd45f999
GabrielWLM <gabrielwlmatienzo@gmail.com>
2021-06-24 17:54:01 +0800

    Add customisability of note and image for blocks, bug where two notes are played, bug where create block plays a note

--- Assets/Scripts/OctaveMenu.cs
+++ Assets/Scripts/OctaveMenu.cs
@@ -7,31 +7,81 @@ public class OctaveMenu : MonoBehaviour
 {
     public void On2Click()
     {
-
+        // char[] cArray =  Audio.lastSelectedNote.ToCharArray();
+        // cArray[cArray.Length - 1] = '2';
+        // Audio.lastSelectedNote = new string(cArray);
+        // AudioSource audioSource = GetComponent<AudioSource>();
+        // AudioClip audioClip = Audio.GetClip("Piano", Audio.lastSelectedNote);
+        // audioSource.clip = audioClip;
+        // audioSource.Play();
     }
 
     public void On3Click()
     {
-
+        char[] cArray =  Audio.lastSelectedNote.ToCharArray();
+        cArray[cArray.Length - 1] = '3';
+        Audio.lastSelectedNote = new string(cArray);
+        AudioSource audioSource = GetComponent<AudioSource>();
+        AudioClip audioClip = Audio.GetClip("Piano", Audio.lastSelectedNote);
+        audioSource.clip = audioClip;
+        audioSource.Play();
     }
 
     public void On4Click()
     {
-
+        char[] cArray =  Audio.lastSelectedNote.ToCharArray();
+        cArray[cArray.Length - 1] = '4';
+        Audio.lastSelectedNote = new string(cArray);
+        AudioSource audioSource = GetComponent<AudioSource>();
+        AudioClip audioClip = Audio.GetClip("Piano", Audio.lastSelectedNote);
+        audioSource.clip = audioClip;
+        audioSource.Play();
     }
 
     public void On5Click()
     {
-
+        char[] cArray =  Audio.lastSelectedNote.ToCharArray();
+        cArray[cArray.Length - 1] = '5';
+        Audio.lastSelectedNote = new string(cArray);
+        AudioSource audioSource = GetComponent<AudioSource>();
+        AudioClip audioClip = Audio.GetClip("Piano", Audio.lastSelectedNote);
+        audioSource.clip = audioClip;
+        audioSource.Play();
     }
 
     public void On6Click()
     {
-
+        // char[] cArray =  Audio.lastSelectedNote.ToCharArray();
+        // cArray[cArray.Length - 1] = '6';
+        // Audio.lastSelectedNote = new string(cArray);
+        // AudioSource audioSource = GetComponent<AudioSource>();
+        // AudioClip audioClip = Audio.GetClip("Piano", Audio.lastSelectedNote);
+        // audioSource.clip = audioClip;
+        // audioSource.Play();
     }
 
      public void OnSharpClick()
     {
-        
+        char[] cArray =  Audio.lastSelectedNote.ToCharArray();
+        if (cArray.Length == 2)
+        {
+            char [] charArray = { cArray[0], '-', cArray[1] };
+            Audio.lastSelectedNote = new string(charArray);
+            GetComponent<Image>().overrideSprite = Resources.Load<Sprite>("Buttons/Sharp");
+            AudioSource audioSource = GetComponent<AudioSource>();
+            AudioClip audioClip = Audio.GetClip("Piano", Audio.lastSelectedNote);
+            audioSource.clip = audioClip;
+            audioSource.Play();
+        }
+        else
+        {
+            char [] charArray2 = { cArray[0], cArray[2] };
+            Audio.lastSelectedNote = new string(charArray2);
+            GetComponent<Image>().overrideSprite = Resources.Load<Sprite>("Buttons/Natural");
+            AudioSource audioSource = GetComponent<AudioSource>();
+            AudioClip audioClip = Audio.GetClip("Piano", Audio.lastSelectedNote);
+            audioSource.clip = audioClip;
+            audioSource.Play();
+        }
     }
 }
